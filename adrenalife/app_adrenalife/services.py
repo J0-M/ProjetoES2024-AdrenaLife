aefdc336e65608526e7a9898f4f9678619f5cf5d
J0-M <joaomarcosavancini00@gmail.com>
2025-02-25 03:15:13 -0300

    Initial Commit

---
+++ adrenalife/app_adrenalife/services.py
@@ -0,0 +1,10 @@
+class CategoriaAtividadeService:
+    @staticmethod
+    def create_categoria(data):
+        # Verificar se a categoria já existe e salvar
+        pass
+    
+    @staticmethod
+    def get_categoria(id=None, nome=None):
+        # Lógica para retornar categoria
+        pass
